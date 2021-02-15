d2100ee8af19b4661c716ce376ab34e5da2739a5
Adam Berkowitz <adam@adamjberkowitz.com>
2021-02-15 16:10:32 -0500

    organizing a base layout and fonts

---
+++ Components/Layout/Layout.js
@@ -0,0 +1,35 @@
+import Link from 'next/link'
+
+export default function Layout({ children }) {
+
+  return (
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/blog">Blog</Link>
+            </li>
+            <li>
+              <Link href="/projects">Projects</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+            <li>
+              <Link href="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      { children }
+      <footer>
+        this is the footer
+      </footer>
+    </>
+  )
+
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import 'prismjs/themes/prism-okaidia.css'
 
+import Layout from '../Components/Layout/Layout'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
