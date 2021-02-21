dc3cf088751fe7cee3c1b194952d212adef4f788
Adam Berkowitz <adam@adamjberkowitz.com>
2021-02-21 12:33:06 -0500

    skip nav

---
+++ Components/Head/SkipNav.js
@@ -0,0 +1,5 @@
+export default function SkipNav() {
+  return (
+    <a id="skip-nav" href="#main">Skip Navigation</a>
+  )
+}
--- Components/Layout/Layout.js
+++ Components/Layout/Layout.js
@@ -1,16 +1,20 @@
+import SkipNav from '../Head/SkipNav'
 import Header from './Header'
 import Footer from './Footer'
 
 export default function Layout({ children }) {
 
   return (
-    <div className="container mx-auto p-4">
-      <Header />
-      <main className="mb-20">
-        { children }
-      </main>
-      <Footer />
-    </div>
+    <>
+      <SkipNav />
+      <div className="container mx-auto p-4">
+        <Header />
+        <main id="main" className="mb-20">
+          { children }
+        </main>
+        <Footer />
+      </div>
+    </>
   )
 
 }
