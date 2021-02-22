914cc09303a2d175266e1042d63eec0b182eaa81
Adam Berkowitz <adam@adamjberkowitz.com>
2021-02-22 15:08:05 -0500

    starting to implement react-helmet

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,29 @@
-import Head from 'next/head'
-
+import { Helmet } from 'react-helmet'
 import "tailwindcss/tailwind.css";
 import '../styles/globals.css'
 import '../styles/custom-prism-theme-dark.css'
 
-import SEO from '../Components/Head/SEO'
 import Layout from '../Components/Layout/Layout'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-    <Head>
-      <SEO 
-        description="Adam Berkowitz is a web developer based in West Hartford Connecticut" 
-      />
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&family=IBM+Plex+Sans:ital,wght@0,300;0,400;0,600;1,300;1,400;1,600&family=IBM+Plex+Serif:wght@700&display=swap" rel="stylesheet" />
-    </Head>
+      <Helmet
+        htmlAttributes={{ lang: 'en' }}
+        title="Adam Berkowitz, Web Developer"
+        description="Adam Berkowitz is a web developer based in West Hartford Connecticut"
+        meta={[
+          {
+            name: 'viewport',
+            content: 'width=device-width, initial-scale=1',
+          },
+          { property: 'og:title', content: 'Adam Berkowitz, Web Developer' },
+        ]}
+        
+      >
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&family=IBM+Plex+Sans:ital,wght@0,300;0,400;0,600;1,300;1,400;1,600&family=IBM+Plex+Serif:wght@700&display=swap" rel="stylesheet" />
+      </Helmet>
     <Layout>
       <Component {...pageProps} />
     </Layout>
